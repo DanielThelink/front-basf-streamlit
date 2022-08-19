437d678444acd7671249fc679183f911029fe9ae
The-Icaro <icarofigaro100@hotmail.com>
2022-08-19 19:42:47 -0300

    remove sentiment analisys page

--- app/pages/SentimentAnalysis.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-from app.components.navbar import navbar
-from app.components.sentiment_analisys import sentiment_analisys
-
-st.sidebar.markdown("# SentimentAnalysis")
-navbar()
-sentiment_analisys()
